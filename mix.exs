d4bbabf5975307ad151634c26f6fb11854a4f854
Timmo Verlaan <tverlaan@gmail.com>
2017-05-08 09:20:00 +0200

    Bump version number to 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule InetTcp_dist.Mixfile do
 
   def project do
     [app: :inet_tcp_dist,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
