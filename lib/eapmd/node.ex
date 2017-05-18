f1ff55cef03281f56b51613514b1d848b7406071
Timmo Verlaan <tverlaan@gmail.com>
2017-05-18 21:40:21 +0200

    Add typespecs to `EAPMD.Node`

--- lib/eapmd/node.ex
+++ lib/eapmd/node.ex
@@ -1,9 +1,28 @@
 defmodule EAPMD.Node do
 	@moduledoc """
-	Struct to descibe a node. It needs to be updated to be of better use.
+	Properties of a node.
+
+    - `:ip` - IP address of the node
+    - `:port` - port that the node is listening on
+    - `:name` - full name of the node, eg. _foo@example_
+    - `:domain` - full domain of the node, eg. _example.local_
 	"""
-  defstruct ip: nil, # ip of node
-            port: -1, # port of node
-            name: nil, # name of node
+
+	@typedoc """
+	IP address
+	"""
+	@type ip_address :: {integer, integer, integer, integer}
+
+	@type t :: %__MODULE__{
+    ip: ip_address,
+    port: integer,
+    name: atom,
+    domain: charlist
+  }
+
+  defstruct ip: nil,
+            port: -1,
+            name: nil,
             domain: nil
+
 end
